de4d15ab01b47724266474ba42ae8f42bd6f1522
MohammadAsadolahi <79268727+MohammadAsadolahi@users.noreply.github.com>
2024-06-17 16:56:51 +0330

    Update Utils.py
    Soft and hard update functions for neural networks in PyTorch has been added

--- Utils.py
+++ Utils.py
@@ -1,10 +1,18 @@
-def set_seed(seed):
-    random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    np.random.seed(seed)
-    T.manual_seed(seed)
-    T.cuda.manual_seed(seed)
-    T.backends.cudnn.deterministic = True
+# def set_seed(seed):
+#     random.seed(seed)
+#     os.environ['PYTHONHASHSEED'] = str(seed)
+#     np.random.seed(seed)
+#     T.manual_seed(seed)
+#     T.cuda.manual_seed(seed)
+#     T.backends.cudnn.deterministic = True
 
 def softmax(x):
     return np.exp(x)/np.sum(np.exp(x), keepdims=True)
+
+def soft_update(target, source, tau):
+	for target_param, param in zip(target.parameters(), source.parameters()):
+		target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
+
+def hard_update(target, source):
+	for target_param, param in zip(target.parameters(), source.parameters()):
+		target_param.data.copy_(param.data)
